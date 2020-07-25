02419019859ce2d4f2b735f62f56739ad1c43653
guanbq <1530942838@qq.com>
2020-07-25 18:37:03 +0800

    spring 原理学习demo

---
+++ spring-study-04/src/test/java/MyTest.java
@@ -0,0 +1,13 @@
+import com.gbq.pojo.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Student student = (Student) context.getBean("student");
+        System.out.println(student.getName());
+        System.out.println(student.getAddress());
+    }
+}
