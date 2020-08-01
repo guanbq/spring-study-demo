b42cad9c18ee037b059eb048bf83f8c117bac449
guanbq <1530942838@qq.com>
2020-08-01 14:48:22 +0800

    mybatis整合Spring

---
+++ spring-study-10-mybatis/src/main/java/com/gbq/mapper/UserMapperImpl.java
@@ -0,0 +1,25 @@
+package com.gbq.mapper;
+
+import com.gbq.pojo.User;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+
+import java.util.List;
+
+public class UserMapperImpl implements UserMapper {
+
+    private SqlSessionTemplate sqlSession;
+
+    public SqlSession getSqlSession() {
+        return sqlSession;
+    }
+
+    public void setSqlSession(SqlSessionTemplate sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    @Override
+    public List<User> selectUser() {
+        return sqlSession.getMapper(UserMapper.class).selectUser();
+    }
+}
--- spring-study-10-mybatis/src/test/java/MyTest.java
+++ spring-study-10-mybatis/src/test/java/MyTest.java
@@ -5,6 +5,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,7 +16,7 @@ public class MyTest {
 
     @Test
     public void test01() throws IOException {
-        String resource = "mybatis-config.xml";
+       /* String resource = "mybatis-config.xml";
         InputStream in = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
@@ -23,6 +25,15 @@ public class MyTest {
         List<User> users = userMapper.selectUser();
         for (User user : users) {
             System.out.println(user);
+        }*/
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+        UserMapper userMapperImpl = context.getBean("userMapperImpl", UserMapper.class);
+        List<User> userList = userMapperImpl.selectUser();
+        for (User user : userList) {
+            System.out.println(user);
         }
+
+
     }
 }
